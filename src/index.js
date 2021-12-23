9f9a93b11a44c7e01eb7cfb987317b68b8ac83bc
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-23 23:48:27 +0200

    middlewares

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@
 require("dotenv").config();
 const express = require("express");
 
+// internal imports
+const sequelize = require("./config/connection");
+// const routes = require('./routes')
+const logger = require("./middlewares/logger");
+
+// setup express port
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+// app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// internal imports
