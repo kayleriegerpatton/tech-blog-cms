74cd67020fec41187ed0d68d9250851691ca9173
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-02 23:33:09 +0000

    session config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // EXTERNAL IMPORTS
 require("dotenv").config();
 const express = require("express");
+const expressSession = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(
+  expressSession.Store
+);
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
 
@@ -8,15 +12,32 @@ const path = require("path");
 const sequelize = require("./config/connection");
 const routes = require("./routes");
 const logger = require("./middlewares/logger");
+const { Sequelize } = require("sequelize/dist");
 
 // declare port
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+// session config
+const session = {
+  secret: "Secret",
+  cookie: {
+    // 24 hours
+    maxAge: 86400000,
+  },
+  resave: false,
+  // false or true?
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    // use same connected db 'sequelize'
+    db: sequelize,
+  }),
+};
+
+app.use(expressSession(session));
 
 // create handlebars instance
 const handlebars = expressHandlebars.create({});
-
-// declare express server app
-const app = express();
 
 // MIDDLEWARES
 // use handlebars as tmeplate engine
