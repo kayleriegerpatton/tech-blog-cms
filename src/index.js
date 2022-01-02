2f33c7d586db2fb2ae4e025d17a8630c49382b9e
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-02 22:26:12 +0000

    basic views w/ handlebars

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const path = require("path");
 
 // INTERNAL IMPORTS
 const sequelize = require("./config/connection");
@@ -22,6 +23,7 @@ const app = express();
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
