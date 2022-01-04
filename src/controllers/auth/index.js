93f8a4f09c22c3afcd198a33093536320b7ac5ab
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-04 00:04:19 +0000

    auth controllers

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,32 +1,103 @@
 // INTERNAL IMPORTS
 const { User } = require("../../models");
 const logError = require("../../utils/logError");
+const getPayloadWithValidFieldsOnly = require("../../utils/payloadValidator");
 
-// import colors?
+// /auth endpoints
 
-// /auth
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  try {
+    // get payload
+    const payload = getPayloadWithValidFieldsOnly(
+      ["password", "email"],
+      req.body
+    );
+
+    // if not all payload fields are present, throw error
+    if (Object.keys(payload).length !== 2) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Please provide the valid fields." });
+    }
+
+    // check if user exists in db
+    const user = await User.findOne({ where: { email: payload.email } });
+    // if user doesn't exist, throw error
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, error: "User does not exist." });
+    }
+
+    // check password (user.checkPassword), throw error
+    const validPassword = await user.checkPassword(payload.password);
+
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ success: false, error: "User not authorized." });
+    }
+
+    // set user session info
+    const userInSession = {
+      id: user.get("id"),
+      email: user.get("email"),
+      username: user.get("username"),
+    };
+    // req.session.save logged in and user session info
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.user = userInSession;
+
+      return res.json({ success: true, message: "Logged in successfully." });
+    });
+  } catch (error) {
+    logError("User login", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to login user." });
+  }
+};
 
 const signup = async (req, res) => {
   try {
-    const newUserData = await User.create({
-      email: req.body.email,
-      password: req.body.password,
-    });
+    // get payload
+    const payload = getPayloadWithValidFieldsOnly(
+      ["username", "password", "email"],
+      req.body
+    );
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+    // if not all payload fields are present, throw error
+    if (Object.keys(payload).length !== 3) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Please provide the valid fields." });
+    }
 
-      res.status(200).json({ message: "User logged in.", user: newUserData });
-    });
+    // create new user record w/ payload
+    await User.create(payload);
+
+    return res.json({ success: true, message: "Created new user." });
   } catch (error) {
     logError("POST user", error.message);
     return res
       .status(500)
-      .json({ success: false, error: "Failed to send response." });
+      .json({ success: false, error: "Failed to create user." });
   }
 };
 
-const logout = (req, res) => {};
+const logout = (req, res) => {
+  // if user is logged in, destroy session
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      return res.json({ success: true, message: "Logged out." });
+    });
+  } else {
+    return res.status(404).json({
+      success: false,
+      error: "User already logged out.",
+    });
+  }
+};
 
 module.exports = { login, signup, logout };
--- src/utils/payloadValidator.js
+++ src/utils/payloadValidator.js
@@ -1,10 +1,11 @@
-// const getPayloadWithValidFieldsOnly = (validFields, payload) =>
-//   Object.entries(payload).reduce(
-//     (acc, [key, value]) =>
-//       validFields.includes(key) ? { ...acc, [key]: value } : acc,
-//     {}
-//   );
+const getPayloadWithValidFieldsOnly = (validFields, payload) =>
+  Object.entries(payload).reduce(
+    (acc, [key, value]) =>
+      // if req.body key is valid, put in payload
+      validFields.includes(key) ? { ...acc, [key]: value } : acc,
+    {}
+  );
 
-// module.exports = {
-//   getPayloadWithValidFieldsOnly,
-// };
+module.exports = {
+  getPayloadWithValidFieldsOnly,
+};
