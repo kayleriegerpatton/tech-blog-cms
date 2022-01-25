8d2d1960f1874db009e98918147b6a59bb7883c8
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-26 00:53:06 +0200

    render empty login field errors

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,12 +3,47 @@ const signupForm = $("#signup-form");
 const loginForm = $("#login-form");
 const logoutBtn = $("#logout-btn");
 
+const renderLoginErrorMessages = (errors) => {
+  const fields = ["email", "password"];
+
+  fields.forEach((field) => {
+    const errorDiv = $(`#${field}-error`);
+
+    if (errors[field]) {
+      errorDiv.text(errors[field]);
+    } else {
+      errorDiv.text("");
+    }
+  });
+};
+
+const getLoginErrors = ({ email, password }) => {
+  const errors = {};
+
+  if (!email) {
+    errors.email = "Invalid email.";
+  }
+
+  if (!password) {
+    errors.password = "Invalid password.";
+  }
+
+  return errors;
+};
+
 const handleLogin = async (event) => {
   event.preventDefault();
 
   // get post body from form fields
   const email = $("#email").val();
   const password = $("#password").val();
+
+  // display empty field errors
+  const errors = getLoginErrors({
+    email,
+    password,
+  });
+  renderLoginErrorMessages(errors);
 
   // make POST request to /auth/login
   const response = await fetch("/auth/login", {
